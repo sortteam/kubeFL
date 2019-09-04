c32a162092a6485fdbaf6ddd1e45447a107a1167
graykode <nlkey2022@gmail.com>
2019-09-04 16:34:37 +0900

    sortteam/HyKuFe#4 Terraform+Ansible로 모든 EC2를 Reboot하는 기능추가

--- client/ansible/restart.sh
+++ client/ansible/restart.sh
@@ -1,4 +1,4 @@
 ansible-playbook -i ./inventory/ec2.py \
       --limit "tag_type_client" \
       -u ubuntu \
-      --private-key ~/.ssh/SoRT.pem ping.yaml -vvvv
+      --private-key ~/.ssh/SoRT.pem restart.yaml -vvvv
