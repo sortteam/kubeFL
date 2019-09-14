d1fcc27bc0c9e3e6951a11b0cd2cd3afa381c81e
graykode <nlkey2022@gmail.com>
2019-09-14 20:14:11 +0900

    sortteam/HyKuFe#9 flask unittest

---
+++ test_aggregator/init.sh
@@ -0,0 +1,10 @@
+sudo swapoff -a
+sudo dd if=/dev/zero of=/swapfile bs=1G count=4
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon -s
+sudo apt update
+sudo apt install -y python3 python3-pip
+pip3 --no-cache-dir install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp35-cp35m-linux_x86_64.whl
+pip3 install image flask
