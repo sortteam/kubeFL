0e453cd3b943ff97a81512d689405121735538be
graykode <nlkey2022@gmail.com>
2019-09-14 23:23:31 +0900

    sortteam/HyKuFe#9 mean calculation 및 Round

--- test_aggregator/init.sh
+++ test_aggregator/init.sh
@@ -7,4 +7,4 @@ sudo swapon -s
 sudo apt update
 sudo apt install -y python3 python3-pip
 pip3 --no-cache-dir install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp35-cp35m-linux_x86_64.whl
-pip3 install image flask
+pip3 install flask
