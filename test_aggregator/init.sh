ee973f49ef6e717d95e2e46f30638d1277391b25
graykode <nlkey2022@gmail.com>
2019-10-31 16:48:16 +0900

    sortteam/HyKuFe#9 fix torchvision problem

--- test_aggregator/init.sh
+++ test_aggregator/init.sh
@@ -6,8 +6,7 @@ sudo swapon /swapfile
 sudo swapon -s
 sudo apt update
 sudo apt install -y python3 python3-pip
-pip3 --no-cache-dir install https://download.pytorch.org/whl/cpu/torch-1.1.0-cp35-cp35m-linux_x86_64.whl
-pip3 --no-cache-dir install https://download.pytorch.org/whl/cpu/torchvision-0.3.0-cp35-cp35m-linux_x86_64.whl
+pip3 install torchvision torch
 pip3 install flask boto3 awscli
 sudo apt install python3-matplotlib -y
 wget https://raw.githubusercontent.com/sortteam/kubeFL/master/test_aggregator/test_flask.py
